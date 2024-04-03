072194f36a9ccaafee47a843e09a4e6cc79f6590
Samuel Monson <smonson@irbash.net>
2024-04-03 15:01:07 -0700

    Update flake to contain cuda deps

--- flake.nix
+++ flake.nix
@@ -9,14 +9,24 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            cudaSupport = true;
+            #cudaCapabilities = [ "7.5" "8.6"  ];
+            cudaForwardCompat = false;
+          };
+        };
       in
       {
         # TODO: Have to override with no CC since we're building with host CUDA
-        devShells.default = pkgs.mkShell.override { stdenv = pkgs.stdenvNoCC; } {
+        #devShells.default = pkgs.mkShell.override { stdenv = pkgs.stdenvNoCC; } {
+        devShells.default = pkgs.mkShell.override { stdenv = pkgs.gcc11Stdenv; } {
           packages = with pkgs; [
             clang-tools # LSP code server
-            #cudaPackages_12_3
+            cudaPackages_12_3.cudatoolkit
+            cudaPackages_12_3.cuda_gdb
           ];
         };
       });
