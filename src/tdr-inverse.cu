13894a8e215ac22c5cac4080d3bac32a5cd25c2c
Samuel Monson <smonson@irbash.net>
2024-04-03 16:04:12 -0700

    Read matrix filenames from stdin and compare to soln

--- src/tdr-inverse.cu
+++ src/tdr-inverse.cu
@@ -1,5 +1,6 @@
 #include "harmonize.cpp"
 #include "util/host.cpp"
+#include <cstdio>
 #include <string>
 #include <fstream>
 #include <sstream>
@@ -8,6 +9,52 @@
 using namespace util;
 
 typedef float matrix_t;
+const int MAX_BLOCK_SIZE = 1024;
+std::string const USAGE = "matrixfile [solnfile]";
+
+// desc: Reports incorrect command-line usage and exits with status 1
+//  pre: None
+// post: In description
+void bad_usage(char* exec) {
+    fprintf(stderr, "Usage: %s %s\n", exec, USAGE.c_str());
+    std::exit(1);
+}
+
+// desc: Returns the value and thread counts provided by the supplied
+//       command-line arguments.
+//  pre: There should be exactly two arguments, both positive integers.
+//       If this precondition is not met, the program will exit with
+//       status 1
+// post: In description
+void get_args(int argc, char *argv[], std::ifstream &matrixFile, std::ifstream &solnFile) {
+    if (argc <= 2) {
+        fprintf(stderr, "Too few args\n");
+        bad_usage(argv[0]);
+    } else if (argc > 3) {
+        fprintf(stderr, "Too many args\n");
+        bad_usage(argv[0]);
+    }
+
+    std::string matrix_filename(argv[1]);
+    // TODO: Allow only one file and guess others name
+    std::string soln_filename(argv[2]);
+
+    if (matrix_filename.compare(soln_filename) == 0) {
+        bad_usage(argv[0]);
+    }
+
+    matrixFile.open(matrix_filename);
+    if (!matrixFile) {
+        fprintf(stderr, "Cannot read file: %s\n", matrix_filename.c_str());
+        bad_usage(argv[0]);
+    }
+    solnFile.open(soln_filename);
+    if (!solnFile) {
+        fprintf(stderr, "Cannot read file: %s\n", soln_filename.c_str());
+        bad_usage(argv[0]);
+    }
+}
+
 
 // Read a comma seperated CSV into memory.
 __host__ void readCSV(std::istream &file, std::vector<matrix_t> &data, size_t &rows, size_t &cols) {
@@ -91,7 +138,7 @@ template <typename T> void copy_from_gpu(T *cpu_array, T *gpu_array, size_t size
 // (c) Sharma 2013
 __global__ void fixRow(matrix_t *matrix, int size, int rowId) {
   // the ith row of the matrix
-  __shared__ matrix_t Ri[512];
+  __shared__ matrix_t Ri[MAX_BLOCK_SIZE];
   // The diagonal element for ith row
   __shared__ matrix_t Aii;
   int colId = threadIdx.x;
@@ -110,11 +157,11 @@ __global__ void fixColumn(matrix_t *matrix, int size, int colId) {
   int i = threadIdx.x;
   int j = blockIdx.x;
   // The colId column
-  __shared__ matrix_t col[512];
+  __shared__ matrix_t col[MAX_BLOCK_SIZE];
   // The jth element of the colId row
   __shared__ matrix_t AColIdj;
   // The jth column
-  __shared__ matrix_t colj[512];
+  __shared__ matrix_t colj[MAX_BLOCK_SIZE];
   col[i] = matrix[i * size + colId];
   if(col[i] != 0) {
     colj[i] = matrix[i * size + j];
@@ -131,16 +178,16 @@ int main(int argc, char *argv[]) {
 
   cli::ArgSet args(argc, argv);
 
-  size_t rows, cols;
+  std::ifstream matrixFile;
+  std::ifstream solnFile;
+  get_args(argc, argv, matrixFile, solnFile);
 
-  std::string filename = "./tests/3x3.csv";
-  std::ifstream file(filename);
+  size_t rows, cols;
+  std::vector<matrix_t> soln;
+  readCSV(solnFile, soln, rows, cols);
+
   std::vector<matrix_t> data;
-  if (!file.is_open()) {
-      printf("Error opening %s\n", filename);
-      return 1;
-  }
-  readCSV(file, data, rows, cols);
+  readCSV(matrixFile, data, rows, cols);
 
   printMatrix(data.data(), rows, cols);
 
@@ -175,7 +222,12 @@ int main(int argc, char *argv[]) {
   data.clear();
   augToMatrix(data, aug, rows, cols);
 
-  printMatrix(data.data(), rows, cols);
+  for (size_t i = 0; i < soln.size(); i++) {
+    if (std::abs(data[i] - soln[i]) > 0.0000001) {
+      fprintf(stderr, "matrix[%zu][%zu] expected % 7f got % 7f\n", i / cols, i % cols, soln[i], data[i]);
+    }
+  }
+  //printMatrix(data.data(), rows, cols);
 
   return 0;
 }
